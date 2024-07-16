92c5de8f63e0be9f092e68e8620357e1a4f738a8
Ramirez0245 <Ramirez0245@yahoo.com>
2024-07-15 18:52:46 -0700

    "Update: added follow def exe_push, push_actions"

--- commit_push.py
+++ commit_push.py
@@ -1,16 +1,33 @@
 import subprocess
 
-placeholder = input("Enter the comment for the commit: ")
-placeholder = '"' + placeholder + '"'
-ACTIONS = [
-    ["git","add", ".", ],
-    ["git", "commit", "-m", placeholder],
-    ["git", "branch", "-M", "main"],
-    ["git", "push", "-u", "origin", "main"]
-    ]
 
-for command in ACTIONS:
-    print(command)
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    print(result.stdout)
 
+def main():
+    command = input("Enter custom 'git' command")
+    match command:
+        case 'push':
+            exe_push()
+        case _:
+            print('Please try')
+
+def exe_push():
+    placeholder = input("Enter the comment for the commit: ")
+    placeholder = '"' + placeholder + '"'
+
+    PUSH = [
+        ["git","add", ".", ],
+        ["git", "commit", "-m", placeholder],
+        ["git", "branch", "-M", "main"],
+        ["git", "push", "-u", "origin", "main"]
+        ]    
+    loop_actions(PUSH)
+    
+
+def loop_actions(command):
+    for actions in command:
+        print(actions)
+        result = subprocess.run(actions, shell=True, capture_output=True, text=True)
+        print(result.stdout)    
+
+if __name__ == '__main__':
+    main()
